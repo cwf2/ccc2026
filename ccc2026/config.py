253797ec77653a98c85761931a679044d63abcf7
Christopher Forstall <cforstall@gmail.com>
2026-06-11 20:45:17 -0300

    replicate figures from Berenice's May presentation

---
+++ ccc2026/config.py
@@ -0,0 +1,10 @@
+'''config.py - global values'''
+
+# local directory for working data
+DATA_DIR = "data"
+
+# local directory for figure output
+PLOT_DIR = "plot"
+
+# remote osf archive for token data
+OSF_PROJECT = "uz6hg"
